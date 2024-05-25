7aa422356c22fa4af5b53024f2664583cd2a1a66
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-05-25 13:51:16 +0530

    laravelProject

---
+++ resources/views/_menu.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('about') }}">About Us</a></li>
+        <li><a href="{{ route('contact') }}">Contact Us</a></li>
+        <!-- Add more menu items as needed -->
+    </ul>
+</nav>
